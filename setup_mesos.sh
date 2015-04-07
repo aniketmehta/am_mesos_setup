93eca755664d264ba9c6791947e34c3e144bcecf
aniketmehta <aniketmehta@gmail.com>
2015-04-07 11:28:35 -0700

    Testing functions1

--- setup_mesos.sh
+++ setup_mesos.sh
@@ -1,5 +1,98 @@
 #!/bin/bash
 
+install_mesos_master(){
+
+	echo '
+	--------------------------------------------------
+	--------------------------------------------------
+	--------------------------------------------------'
+	echo "starting master setup:"
+	
+    echo '
+	--------------------------------------------------
+	--------------------------------------------------
+	--------------------------------------------------'
+	
+	## Mesos master setup now.
+	
+	#sudo apt-get install mesosphere
+	
+	#echo "zk://"${server_ip_1}":2181,"${server_ip_2}":2181,"${server_ip_3}":2181/mesos" | tee /etc/mesos/zk
+	
+	#if [ "$thisserverip" == "$server_ip_1" ]
+	#then
+		echo $server_id_1 | tee /etc/zookeeper/conf/myid		
+	#elif [ "$thisserverip" == "$server_ip_2" ]
+	#then
+	#	echo $server_id_2 | tee /etc/zookeeper/conf/myid
+	#elif [ "$thisserverip" == "$server_ip_3" ]
+	#then
+	#	echo $server_id_3 | tee /etc/zookeeper/conf/myid
+	#fi
+	
+	#echo "server."${server_id_1}"="${server_ip_1}":2888:3888" | tee -a /etc/zookeeper/conf/zoo.cfg
+	#echo "server."${server_id_2}"="${server_ip_2}":2888:3888" | tee -a /etc/zookeeper/conf/zoo.cfg
+	#echo "server."${server_id_3}"="${server_ip_3}":2888:3888" | tee -a /etc/zookeeper/conf/zoo.cfg
+	
+	#echo ${server_quorum} | tee /etc/mesos-master/quorum
+	
+	#echo ${thisserverip} | tee /etc/mesos-master/ip
+	
+	#cp /etc/mesos-master/ip /etc/mesos-master/hostname
+	
+	#mkdir -p /etc/marathon/conf
+	
+	#cp /etc/mesos-master/hostname /etc/marathon/conf
+	
+	#cp /etc/mesos/zk /etc/marathon/conf/master
+	
+	#cp /etc/marathon/conf/master /etc/marathon/conf/zk
+	
+	#echo "zk://"${server_ip_1}":2181,"${server_ip_2}":2181,"${server_ip_3}":2181/marathon" | tee /etc/marathon/conf/zk
+	
+	#stop mesos-slave
+	
+	#echo manual | sudo tee /etc/init/mesos-slave.override
+	
+	#restart zookeeper
+	
+	#start mesos-master
+	
+	#start marathon
+}
+
+install_mesos_slave(){
+	echo '
+	--------------------------------------------------
+	--------------------------------------------------
+	--------------------------------------------------'
+	
+	echo "starting slave setup:"
+
+    echo '
+	--------------------------------------------------
+	--------------------------------------------------
+	--------------------------------------------------'
+	
+	##Slave set up now.
+	
+	#apt-get install mesos
+	
+	#echo "zk://"${server_ip_1}":2181,"${server_ip_2}":2181,"${server_ip_3}":2181/mesos" | tee /etc/mesos/zk
+	
+	#stop zookeeper
+	#echo manual | sudo tee /etc/init/zookeeper.override
+	
+	#echo manual | sudo tee /etc/init/mesos-master.override
+	#stop mesos-master
+	
+	#echo ${thisserverip} | sudo tee /etc/mesos-slave/ip
+	#cp /etc/mesos-slave/ip /etc/mesos-slave/hostname
+	
+	#start mesos-slave
+	
+}
+
 thisserverip="$(hostname -i)"
 echo "this server ip: ${thisserverip}"
 
@@ -11,7 +104,7 @@ echo " Please Type
 read input
 
 
-if [ $input == 1 ]
+if [ $input == 1 ];
 then
   file="./configuration.txt"
 
@@ -20,22 +113,33 @@ then
   	while read -r line
   	do
    		read -ra words <<< ${line}
-   		if [ "$thisserverip" == "$words" ]
+ 		if [ "$thisserverip" == "$words" ]
 		then
-   			if [ 'master' == "${words[1]}"]
+   			if [ "${words[1]}" == 'master']
    			then
    				echo "master found ID: "${words[2]}
-   				master=1
-   			elif ['slave' == "${words[1]}"]
+   				install_master="1"
+   			elif [ "${words[1]}" == 'slave']
    			then 
    				echo "slave found"
-   				master=0
-   			else
-   				echo "This host is not listed in configuraion file. Please check the error. This Host: ${thisserverip}"	
-   			fi 
-        fi 
+   				install_master="0"
+   		else
+   			echo "This host is not listed in configuraion file. Please check the error. This Host: ${thisserverip}"	
+   		fi 
+    fi 
+    
+ 	
+ 	
  	done <configuration.txt
 
+	for (( i=0; i<${#words[@]}; i++))
+	do
+		echo -n "${words[i]}"
+    	if [ "$i" == "master" ] ; then
+        	echo "Found"
+    	fi
+	done
+    	
   else
    echo "configuration.txt file not found. Configuration file is created for you. Please take a look at update it accordingly."
    echo '192.168.0.1 master 1
@@ -95,95 +199,11 @@ fi
 	--------------------------------------------------
 	--------------------------------------------------'
 	
-if [ $master eq 1 ]
+if [ "${install_master}" == "1" ];
 then 
-    echo '
-	--------------------------------------------------
-	--------------------------------------------------
-	--------------------------------------------------'
-	echo "starting master setup:"
-	
-    echo '
-	--------------------------------------------------
-	--------------------------------------------------
-	--------------------------------------------------'
-	
-	## Mesos master setup now.
-	
-	#sudo apt-get install mesosphere
-	
-	#echo "zk://"${server_ip_1}":2181,"${server_ip_2}":2181,"${server_ip_3}":2181/mesos" | tee /etc/mesos/zk
-	
-	#if [ "$thisserverip" == "$server_ip_1" ]
-	#then
-		echo $server_id_1 | tee /etc/zookeeper/conf/myid		
-	#elif [ "$thisserverip" == "$server_ip_2" ]
-	#then
-	#	echo $server_id_2 | tee /etc/zookeeper/conf/myid
-	#elif [ "$thisserverip" == "$server_ip_3" ]
-	#then
-	#	echo $server_id_3 | tee /etc/zookeeper/conf/myid
-	#fi
-	
-	#echo "server."${server_id_1}"="${server_ip_1}":2888:3888" | tee -a /etc/zookeeper/conf/zoo.cfg
-	#echo "server."${server_id_2}"="${server_ip_2}":2888:3888" | tee -a /etc/zookeeper/conf/zoo.cfg
-	#echo "server."${server_id_3}"="${server_ip_3}":2888:3888" | tee -a /etc/zookeeper/conf/zoo.cfg
-	
-	#echo ${server_quorum} | tee /etc/mesos-master/quorum
-	
-	#echo ${thisserverip} | tee /etc/mesos-master/ip
-	
-	#cp /etc/mesos-master/ip /etc/mesos-master/hostname
-	
-	#mkdir -p /etc/marathon/conf
-	
-	#cp /etc/mesos-master/hostname /etc/marathon/conf
-	
-	#cp /etc/mesos/zk /etc/marathon/conf/master
-	
-	#cp /etc/marathon/conf/master /etc/marathon/conf/zk
-	
-	#echo "zk://"${server_ip_1}":2181,"${server_ip_2}":2181,"${server_ip_3}":2181/marathon" | tee /etc/marathon/conf/zk
-	
-	#stop mesos-slave
-	
-	#echo manual | sudo tee /etc/init/mesos-slave.override
-	
-	#restart zookeeper
-	
-	#start mesos-master
-	
-	#start marathon
+		install_mesos_master 
 
-elif [ $master eq 0 ]
-
-    echo '
-	--------------------------------------------------
-	--------------------------------------------------
-	--------------------------------------------------'
-	
-	echo "starting slave setup:"
-
-    echo '
-	--------------------------------------------------
-	--------------------------------------------------
-	--------------------------------------------------'
-	
-	##Slave set up now.
-	
-	#apt-get install mesos
-	
-	#echo "zk://"${server_ip_1}":2181,"${server_ip_2}":2181,"${server_ip_3}":2181/mesos" | tee /etc/mesos/zk
-	
-	#stop zookeeper
-	#echo manual | sudo tee /etc/init/zookeeper.override
-	
-	#echo manual | sudo tee /etc/init/mesos-master.override
-	#stop mesos-master
-	
-	#echo ${thisserverip} | sudo tee /etc/mesos-slave/ip
-	#cp /etc/mesos-slave/ip /etc/mesos-slave/hostname
-	
-	#start mesos-slave
-
+elif [ "${install_master}" == "0" ];
+then
+		install_mesos_slave 
 fi
